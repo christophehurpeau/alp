2cd7d673ad2922b3c73ad927ea1c722ffda76aa8
Christophe Hurpeau <christophe@hurpeau.com>
2016-10-18 13:28:33 +0200

    use babili

--- packages/alp-dev/webpack.config.js
+++ packages/alp-dev/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 const OfflinePlugin = require('offline-plugin');
 const createBabelOpts = require('pob-babel/lib/babel-options');
+const BabiliCustomPlugin = require('./BabiliCustomWebpackPlugin');
 
 const production = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production';
 const dest = process.env.WEBPACK_DEST || 'modern-browsers';
@@ -10,135 +11,171 @@ const modernBrowsers = dest === 'modern-browsers';
 // const modernBrowsers = false;
 
 const babelOptions = createBabelOpts(
-    `webpack${modernBrowsers ? '-modern-browsers' : ''}${!production ? '-dev' : ''}`,
-    true
+  `webpack${modernBrowsers ? '-modern-browsers' : ''}${!production ? '-dev' : ''}`,
+  true
 );
 
+const resolvePreset = presetName => require.resolve(`babel-preset-${presetName}`);
+const resolvePlugin = pluginName => require.resolve(`babel-plugin-${pluginName}`);
+const resolveBabel = resolve => thing => {
+  if (Array.isArray(thing)) return [resolveBabel(resolve)(thing[0])].concat(thing.slice(1));
+  if (typeof thing === 'string' && !thing.startsWith('/')) return resolve(thing);
+  return thing;
+};
+
 const modulesList = (() => {
-    try {
-        return fs.readdirSync(path.resolve('src/modules'));
-    } catch (e) {
-        return [];
-    }
+  try {
+    return fs.readdirSync(path.resolve('src/modules'));
+  } catch (e) {
+    return [];
+  }
 })();
 
 module.exports = {
-    devtool: production ? undefined : 'cheap-source-map',
-    bail: true,
+  devtool: production ? undefined : 'cheap-source-map',
+  bail: true,
 
-    entry: {
-        [dest]: [
-            !modernBrowsers && 'babel-regenerator-runtime',
-            !production && 'webpack-hot-middleware/client',
-            !production && 'react-hot-loader/patch',
-            './src/index.browser.js',
-        ].filter(Boolean),
-    },
-    output: {
-        path: path.resolve('public'),
-        publicPath: '/',
-        filename: '[name].js',
-        pathinfo: !production,
-    },
-    module: {
-        // Disable handling of unknown requires
-        unknownContextRegExp: /$^/,
-        unknownContextCritical: false,
-
-        // Disable handling of requires with a single expression
-        exprContextRegExp: /$^/,
-        exprContextCritical: false,
-
-        // Disable handling of expression in require
-        wrappedContextRegExp: /$^/,
-        wrappedContextCritical: false,
-        wrappedContextRecursive: false,
-
-
-        // preLoaders: [
-            // { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
-            // { test: /\.jsx?$/, loader: 'source-map', exclude: /react-hot-loader/ }
-        // ],
-
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: path.resolve('src'),
-                exclude: /\.server\.jsx?$/,
-                use: [
-                    {
-                        loader: 'babel',
-                        options: {
-                            compact: production,
-                            minified: production,
-                            comments: !production,
-                            presets: babelOptions.presets,
-                            plugins: babelOptions.plugins,
-                        },
-                    }
-                ]
-            },
-        ],
-    },
-    resolveLoader: {
-        modules: ['node_modules'],
-    },
-    resolve: {
-        alias: { 'socket.io': 'socket.io-client' },
-        modules: ['node_modules'],
-        extensions: ['.browser.js', '.js', '.browser.jsx', '.jsx', '.json'],
-        mainFields: [
-            modernBrowsers && !production && 'webpack:main-modern-browsers-dev',
-            modernBrowsers && 'webpack:main-modern-browsers',
-            !production && 'webpack:main-dev',
-            'webpack:main',
-            !production && 'browser-dev',
-            'browser',
-            !production && 'main-dev',
-            'webpack',
-            'main',
-        ].filter(Boolean),
-        aliasFields: [
-            modernBrowsers && !production && 'webpack:aliases-modern-browsers-dev',
-            modernBrowsers && 'webpack:aliases-modern-browsers',
-            !production && 'webpack:aliases-dev',
-            'webpack:aliases',
-            'webpack',
-            'browser',
-        ].filter(Boolean),
-    },
-
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: dest,
-            filename: `${dest}.js`,
-            chunks: [dest],
-            // minChunks: modulesList.length === 1 ? 1 : 2,
-        }),
-        new webpack.LoaderOptionsPlugin({
-            debug: !production,
-            minimize: production,
-        }),
-        new webpack.DefinePlugin({
-            BROWSER: true,
-            SERVER: false,
-            NODEJS: false,
-            PRODUCTION: production,
-            MODERN_BROWSERS: modernBrowsers,
-            'process.env': {
-                NODE_ENV: JSON.stringify(production ? 'production' : process.env.NODE_ENV),
-            },
-        }),
-        !production && new webpack.HotModuleReplacementPlugin(),
-        !production && new webpack.NoErrorsPlugin(),
-        production && new webpack.optimize.UglifyJsPlugin({
-            mangle: false,
-            compress: {
-                warnings: false,
-            },
-            sourceMap: !production,
-        }),
-
-        // TODO https://github.com/NekR/offline-plugin
+  entry: {
+    [dest]: [
+      !modernBrowsers && 'babel-regenerator-runtime',
+      !production && 'webpack-hot-middleware/client',
+      !production && 'react-hot-loader/patch',
+      './src/index.browser.js',
     ].filter(Boolean),
+  },
+  output: {
+    path: path.resolve('public'),
+    publicPath: '/',
+    filename: '[name].js',
+    pathinfo: !production,
+  },
+  module: {
+    // Disable handling of unknown requires
+    unknownContextRegExp: /$^/,
+    unknownContextCritical: false,
+
+    // Disable handling of requires with a single expression
+    exprContextRegExp: /$^/,
+    exprContextCritical: false,
+
+    // Disable handling of expression in require
+    wrappedContextRegExp: /$^/,
+    wrappedContextCritical: false,
+    wrappedContextRecursive: false,
+
+
+    // preLoaders: [
+    // { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
+    // { test: /\.jsx?$/, loader: 'source-map', exclude: /react-hot-loader/ }
+    // ],
+
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve('src'),
+        exclude: /\.server\.jsx?$/,
+        use: [
+          {
+            loader: 'babel',
+            options: {
+              compact: production,
+              minified: production,
+              comments: !production,
+              presets: babelOptions.presets.map(resolveBabel(resolvePreset)),
+              plugins: babelOptions.plugins.map(resolveBabel(resolvePlugin)),
+            },
+          },
+        ],
+      },
+    ].filter(Boolean),
+  },
+  resolveLoader: {
+    modules: ['node_modules'],
+  },
+  resolve: {
+    alias: { 'socket.io': 'socket.io-client' },
+    modules: ['node_modules'],
+    extensions: ['.browser.js', '.js', '.browser.jsx', '.jsx', '.json'],
+    mainFields: [
+      modernBrowsers && !production && 'webpack:main-modern-browsers-dev',
+      modernBrowsers && 'webpack:main-modern-browsers',
+      !production && 'webpack:main-dev',
+      'webpack:main',
+      !production && 'browser-dev',
+      'browser',
+      !production && 'main-dev',
+      'webpack',
+      'main',
+    ].filter(Boolean),
+    aliasFields: [
+      modernBrowsers && !production && 'webpack:aliases-modern-browsers-dev',
+      modernBrowsers && 'webpack:aliases-modern-browsers',
+      !production && 'webpack:aliases-dev',
+      'webpack:aliases',
+      'webpack',
+      'browser',
+    ].filter(Boolean),
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: dest,
+      filename: `${dest}.js`,
+      chunks: [dest],
+      // minChunks: modulesList.length === 1 ? 1 : 2,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      debug: !production,
+      minimize: production,
+    }),
+    new webpack.DefinePlugin({
+      BROWSER: true,
+      SERVER: false,
+      NODEJS: false,
+      PRODUCTION: production,
+      MODERN_BROWSERS: modernBrowsers,
+      'process.env': {
+        NODE_ENV: JSON.stringify(production ? 'production' : process.env.NODE_ENV),
+      },
+    }),
+    !production && new webpack.HotModuleReplacementPlugin(),
+    !production && new webpack.NoErrorsPlugin(),
+    production && !modernBrowsers && new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: {
+        warnings: false,
+      },
+      sourceMap: !production,
+    }),
+    production && modernBrowsers && new BabiliCustomPlugin({
+      comments: false,
+      plugins: [
+        [
+          'minify-replace',
+          [
+            ['BROWSER', true],
+            ['SERVER', false],
+            ['NODEJS', false],
+            ['PRODUCTION', production],
+            ['MODERN_BROWSERS', modernBrowsers],
+          ].map(([key, value]) => ({
+            identifierName: key,
+            replacement: { type: 'booleanLiteral', value: value },
+          })),
+        ],
+        'minify-dead-code-elimination',
+        'minify-guarded-expressions',
+        'minify-constant-folding',
+        'minify-flip-comparisons',
+        'minify-simplify',
+        'minify-empty-function',
+        'transform-member-expression-literals',
+        'transform-merge-sibling-variables',
+        'transform-property-literals',
+        'transform-remove-debugger',
+        'minify-mangle-names',
+      ].map(resolveBabel(resolvePlugin)),
+    }),
+    // TODO https://github.com/NekR/offline-plugin
+  ].filter(Boolean),
 };
