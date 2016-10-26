bc90e0452df580ff4d44fd4f057b2557afa56eab
Christophe Hurpeau <christophe@hurpeau.com>
2016-10-26 23:12:07 +0200

    refactor: pob update and update dependencies

--- packages/alp-errors-node/lib-node6/index.js
+++ packages/alp-errors-node/lib-node6/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _http = require('http');
@@ -18,67 +18,67 @@ var _nightingaleLogger2 = _interopRequireDefault(_nightingaleLogger);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 const logger = new _nightingaleLogger2.default('alp.errors');
 const errorHtmlRenderer = new _HtmlRenderer2.default();
 
 exports.default = (() => {
-    var _ref = _asyncToGenerator(function* (ctx, next) {
-        try {
-            yield next();
-        } catch (err) {
-            // eslint-disable-next-line no-ex-assign
-            if (!err) err = new Error('Unknown error');
-            // eslint-disable-next-line no-ex-assign
-            if (typeof err === 'string') err = new Error(err);
+  var _ref = _asyncToGenerator(function* (ctx, next) {
+    try {
+      yield next();
+    } catch (err) {
+      // eslint-disable-next-line no-ex-assign
+      if (!err) err = new Error('Unknown error');
+      // eslint-disable-next-line no-ex-assign
+      if (typeof err === 'string') err = new Error(err);
 
-            ctx.status = err.status || 500;
-            logger.error(err);
+      ctx.status = err.status || 500;
+      logger.error(err);
 
-            switch (ctx.accepts('html', 'text', 'json')) {
-                case 'text':
-                    ctx.type = 'text/plain';
-                    if (process.env.NODE_ENV !== 'production') {
-                        ctx.body = err.message;
-                    } else if (err.expose) {
-                        ctx.body = err.message;
-                    } else {
-                        throw err;
-                    }
+      switch (ctx.accepts('html', 'text', 'json')) {
+        case 'text':
+          ctx.type = 'text/plain';
+          if (process.env.NODE_ENV !== 'production') {
+            ctx.body = err.message;
+          } else if (err.expose) {
+            ctx.body = err.message;
+          } else {
+            throw err;
+          }
 
-                    break;
+          break;
 
-                case 'json':
-                    ctx.type = 'application/json';
-                    if (process.env.NODE_ENV !== 'production') {
-                        ctx.body = { error: err.message };
-                    } else if (err.expose) {
-                        ctx.body = { error: err.message };
-                    } else {
-                        ctx.body = { error: _http.STATUS_CODES[ctx.status] };
-                    }
+        case 'json':
+          ctx.type = 'application/json';
+          if (process.env.NODE_ENV !== 'production') {
+            ctx.body = { error: err.message };
+          } else if (err.expose) {
+            ctx.body = { error: err.message };
+          } else {
+            ctx.body = { error: _http.STATUS_CODES[ctx.status] };
+          }
 
-                    break;
+          break;
 
-                case 'html':
-                    ctx.type = 'text/html';
-                    if (process.env.NODE_ENV !== 'production') {
-                        const parsedError = (0, _alouette.parse)(err);
-                        ctx.body = errorHtmlRenderer.render(parsedError);
-                    } else if (err.expose) {
-                        ctx.body = err.message;
-                    } else {
-                        throw err;
-                    }
+        case 'html':
+          ctx.type = 'text/html';
+          if (process.env.NODE_ENV !== 'production') {
+            const parsedError = (0, _alouette.parse)(err);
+            ctx.body = errorHtmlRenderer.render(parsedError);
+          } else if (err.expose) {
+            ctx.body = err.message;
+          } else {
+            throw err;
+          }
 
-                    break;
-            }
-        }
-    });
+          break;
+      }
+    }
+  });
 
-    return function (_x, _x2) {
-        return _ref.apply(this, arguments);
-    };
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
 })();
 //# sourceMappingURL=index.js.map
--- packages/alp-errors-node/src/index.js
+++ packages/alp-errors-node/src/index.js
@@ -7,54 +7,54 @@ const logger = new Logger('alp.errors');
 const errorHtmlRenderer = new ErrorHtmlRenderer();
 
 export default async function (ctx, next) {
-    try {
-        await next();
-    } catch (err) {
-        // eslint-disable-next-line no-ex-assign
-        if (!err) err = new Error('Unknown error');
-        // eslint-disable-next-line no-ex-assign
-        if (typeof err === 'string') err = new Error(err);
+  try {
+    await next();
+  } catch (err) {
+    // eslint-disable-next-line no-ex-assign
+    if (!err) err = new Error('Unknown error');
+    // eslint-disable-next-line no-ex-assign
+    if (typeof err === 'string') err = new Error(err);
 
-        ctx.status = err.status || 500;
-        logger.error(err);
+    ctx.status = err.status || 500;
+    logger.error(err);
 
-        switch (ctx.accepts('html', 'text', 'json')) {
-            case 'text':
-                ctx.type = 'text/plain';
-                if (process.env.NODE_ENV !== 'production') {
-                    ctx.body = err.message;
-                } else if (err.expose) {
-                    ctx.body = err.message;
-                } else {
-                    throw err;
-                }
-
-                break;
-
-            case 'json':
-                ctx.type = 'application/json';
-                if (process.env.NODE_ENV !== 'production') {
-                    ctx.body = { error: err.message };
-                } else if (err.expose) {
-                    ctx.body = { error: err.message };
-                } else {
-                    ctx.body = { error: STATUS_CODES[ctx.status] };
-                }
-
-                break;
-
-            case 'html':
-                ctx.type = 'text/html';
-                if (process.env.NODE_ENV !== 'production') {
-                    const parsedError = parseError(err);
-                    ctx.body = errorHtmlRenderer.render(parsedError);
-                } else if (err.expose) {
-                    ctx.body = err.message;
-                } else {
-                    throw err;
-                }
-
-                break;
+    switch (ctx.accepts('html', 'text', 'json')) {
+      case 'text':
+        ctx.type = 'text/plain';
+        if (process.env.NODE_ENV !== 'production') {
+          ctx.body = err.message;
+        } else if (err.expose) {
+          ctx.body = err.message;
+        } else {
+          throw err;
         }
+
+        break;
+
+      case 'json':
+        ctx.type = 'application/json';
+        if (process.env.NODE_ENV !== 'production') {
+          ctx.body = { error: err.message };
+        } else if (err.expose) {
+          ctx.body = { error: err.message };
+        } else {
+          ctx.body = { error: STATUS_CODES[ctx.status] };
+        }
+
+        break;
+
+      case 'html':
+        ctx.type = 'text/html';
+        if (process.env.NODE_ENV !== 'production') {
+          const parsedError = parseError(err);
+          ctx.body = errorHtmlRenderer.render(parsedError);
+        } else if (err.expose) {
+          ctx.body = err.message;
+        } else {
+          throw err;
+        }
+
+        break;
     }
+  }
 }
