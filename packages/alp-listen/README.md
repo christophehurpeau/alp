11e2f76d9c0d01061a26fcba4a78f48f783817af
Christophe Hurpeau <christophe@hurpeau.com>
2016-10-26 23:07:43 +0200

    refactor: pob and update dependencies

--- packages/alp-listen/README.md
+++ packages/alp-listen/README.md
@@ -1,6 +1,17 @@
-# alp-listen
+# alp-listen [![NPM version][npm-image]][npm-url]
 
-Handles http and https via config
+Listen function, using config and logger for alp server
+
+[![Dependency ci Status][dependencyci-image]][dependencyci-url]
+[![Dependency Status][daviddm-image]][daviddm-url]
+
+## Install
+
+```sh
+npm install --save alp-listen
+```
+
+## Usage
 
 ```js
 import Koa from 'koa';
@@ -26,3 +37,10 @@ config:
 
 If `tls` is true, the files `server.key` and `server.crt`
 will be loaded from the path given in the first param
+
+[npm-image]: https://img.shields.io/npm/v/alp-listen.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/alp-listen
+[daviddm-image]: https://david-dm.org/alpjs/alp-listen.svg?style=flat-square
+[daviddm-url]: https://david-dm.org/alpjs/alp-listen
+[dependencyci-image]: https://dependencyci.com/github/alpjs/alp-listen/badge?style=flat-square
+[dependencyci-url]: https://dependencyci.com/github/alpjs/alp-listen
--- packages/alp-listen/src/index.js
+++ packages/alp-listen/src/index.js
@@ -8,56 +8,54 @@ const logger = new Logger('alp.listen');
  * @returns {Function}
  */
 export default function alpListen(dirname) {
-    /**
-     * @returns {Promise}
-     */
-    return app => {
-        return new Promise((resolve) => {
-            const socketPath = app.config.get('socketPath');
-            const port = app.config.get('port');
-            const hostname = app.config.get('hostname');
-            const tls = app.config.get('tls');
-            const createServer = require(!socketPath && tls ? 'https' : 'http').createServer;
+  return (app) => (
+    new Promise((resolve) => {
+      const socketPath = app.config.get('socketPath');
+      const port = app.config.get('port');
+      const hostname = app.config.get('hostname');
+      const tls = app.config.get('tls');
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+      const createServer = require(!socketPath && tls ? 'https' : 'http').createServer;
 
-            logger.info(
+      logger.info(
                 'Creating server',
                 socketPath ? { socketPath: socketPath } : { port: port },
-                { [socketPath ? 'socketPath' : 'port']: ['yellow'] }
+                { [socketPath ? 'socketPath' : 'port']: ['yellow'] },
             );
 
-            const server = (() => {
-                if (!tls) {
-                    return createServer(app.callback());
-                }
+      const server = (() => {
+        if (!tls) {
+          return createServer(app.callback());
+        }
 
-                const options = {
-                    key: readFileSync(`${dirname}/server.key`),
-                    cert: readFileSync(`${dirname}/server.crt`),
-                };
+        const options = {
+          key: readFileSync(`${dirname}/server.key`),
+          cert: readFileSync(`${dirname}/server.crt`),
+        };
 
-                return createServer(options, app.callback());
-            })();
+        return createServer(options, app.callback());
+      })();
 
-            if (socketPath) {
-                try {
-                    unlinkSync(socketPath);
-                } catch (err) {
-                }
+      if (socketPath) {
+        try {
+          unlinkSync(socketPath);
+        } catch (err) {
+        }
 
-                server.listen(socketPath, () => {
-                    if (socketPath) {
-                        chmodSync(socketPath, '777');
-                    }
+        server.listen(socketPath, () => {
+          if (socketPath) {
+            chmodSync(socketPath, '777');
+          }
 
-                    logger.info('Server listening', { socketPath }, { socketPath: ['yellow'] });
-                    resolve(server);
-                });
-            } else {
-                server.listen(port, hostname, () => {
-                    logger.info('Server listening', { port }, { port: ['yellow'] });
-                    resolve(server);
-                });
-            }
+          logger.info('Server listening', { socketPath }, { socketPath: ['yellow'] });
+          resolve(server);
         });
-    };
+      } else {
+        server.listen(port, hostname, () => {
+          logger.info('Server listening', { port }, { port: ['yellow'] });
+          resolve(server);
+        });
+      }
+    })
+  );
 }
