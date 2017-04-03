d0671ef11685027a9b91df67b76bdc414fe32bfc
Christophe Hurpeau <christophe@hurpeau.com>
2017-04-03 22:54:45 +0200

    chore: pob upgrade

--- packages/alp-config/lib-module-dev/index.js
+++ packages/alp-config/lib-module-dev/index.js
@@ -2,7 +2,52 @@ var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = [
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _dec, _dec2, _dec3, _desc, _value, _class, _descriptor, _descriptor2, _descriptor3;
+
+function _initDefineProp(target, property, descriptor, context) {
+  if (!descriptor) return;
+  Object.defineProperty(target, property, {
+    enumerable: descriptor.enumerable,
+    configurable: descriptor.configurable,
+    writable: descriptor.writable,
+    value: descriptor.initializer ? descriptor.initializer.call(context) : void 0
+  });
+}
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+  var desc = {};
+  Object['keys'](descriptor).forEach(function (key) {
+    desc[key] = descriptor[key];
+  });
+  desc.enumerable = !!desc.enumerable;
+  desc.configurable = !!desc.configurable;
+
+  if ('value' in desc || desc.initializer) {
+    desc.writable = true;
+  }
+
+  desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+    return decorator(target, property, desc) || desc;
+  }, desc);
+
+  if (context && desc.initializer !== void 0) {
+    desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+    desc.initializer = undefined;
+  }
+
+  if (desc.initializer === void 0) {
+    Object['defineProperty'](target, property, desc);
+    desc = null;
+  }
+
+  return desc;
+}
+
+function _initializerWarningHelper() {
+  throw new Error('Decorating class property failed. Please ensure that transform-class-properties is enabled.');
+}
 
 import { deprecate } from 'util';
 import argv from 'minimist-argv';
@@ -10,18 +55,46 @@ import deepFreeze from 'deep-freeze-es6';
 import parseJSON from 'parse-json-object-as-map';
 import { existsSync, readFileSync } from 'fs';
 
+import t from 'flow-runtime';
 function _existsConfigSync(dirname, name) {
+  var _dirnameType = t.string();
+
+  var _nameType = t.string();
+
+  t.param('dirname', _dirnameType).assert(dirname);
+  t.param('name', _nameType).assert(name);
+
   return existsSync('' + dirname + name + '.json');
 }
 
 function _loadConfigSync(dirname, name) {
+  var _dirnameType2 = t.string();
+
+  var _nameType2 = t.string();
+
+  t.param('dirname', _dirnameType2).assert(dirname);
+  t.param('name', _nameType2).assert(name);
+
   var content = readFileSync('' + dirname + name + '.json');
   return parseJSON(content);
 }
 
-export var Config = function () {
+var ConfigOptions = t.type('ConfigOptions', t.exactObject(t.property('argv', t.nullable(t.array(t.string()))), t.property('packageConfig', t.nullable(t.object())), t.property('version', t.nullable(t.string()))));
+
+
+export var Config = (_dec = t.decorate(t.ref('Map', t.string(), t.any())), _dec2 = t.decorate(t.string()), _dec3 = t.decorate(t.object()), (_class = function () {
   function Config(dirname) {
     _classCallCheck(this, Config);
+
+    _initDefineProp(this, '_map', _descriptor, this);
+
+    _initDefineProp(this, '_dirname', _descriptor2, this);
+
+    _initDefineProp(this, 'packageConfig', _descriptor3, this);
+
+    var _dirnameType3 = t.string();
+
+    t.param('dirname', _dirnameType3).assert(dirname);
 
     this._map = new Map();
     this._dirname = dirname.replace(/\/*$/, '/');
@@ -31,6 +104,10 @@ export var Config = function () {
     key: 'loadSync',
     value: function loadSync() {
       var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+      var _returnType = t.return(t.ref('Map', t.string(), t.any()));
+
+      t.param('options', ConfigOptions).assert(options);
 
       var env = process.env.CONFIG_ENV || process.env.NODE_ENV || 'development';
       var _options$argv = options.argv,
@@ -48,9 +125,12 @@ export var Config = function () {
 
       try {
         for (var _iterator = this.loadConfigSync(env)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var _step$value = _slicedToArray(_step.value, 2),
-              key = _step$value[0],
-              value = _step$value[1];
+          var _ref = _step.value;
+
+          var _ref2 = _slicedToArray(_ref, 2);
+
+          var key = _ref2[0];
+          var value = _ref2[1];
 
           config.set(key, value);
         }
@@ -77,11 +157,14 @@ export var Config = function () {
 
         try {
           for (var _iterator2 = this.loadConfigSync('local')[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var _step2$value = _slicedToArray(_step2.value, 2),
-                key = _step2$value[0],
-                value = _step2$value[1];
+            var _ref3 = _step2.value;
 
-            config.set(key, value);
+            var _ref4 = _slicedToArray(_ref3, 2);
+
+            var _key = _ref4[0];
+            var _value2 = _ref4[1];
+
+            config.set(_key, _value2);
           }
         } catch (err) {
           _didIteratorError2 = true;
@@ -130,34 +213,70 @@ export var Config = function () {
         }
       });
 
-      return this._map = deepFreeze(config);
+      return _returnType.assert(this._map = deepFreeze(config));
     }
   }, {
     key: 'get',
     value: function get(key) {
-      return this._map.get(key);
+      var _keyType = t.string();
+
+      var _returnType2 = t.return(t.any());
+
+      t.param('key', _keyType).assert(key);
+
+      return _returnType2.assert(this._map.get(key));
     }
   }, {
     key: 'existsConfigSync',
     value: function existsConfigSync(name) {
-      return _existsConfigSync(this._dirname, name);
+      var _nameType3 = t.string();
+
+      var _returnType3 = t.return(t.boolean());
+
+      t.param('name', _nameType3).assert(name);
+
+      return _returnType3.assert(_existsConfigSync(this._dirname, name));
     }
   }, {
     key: 'loadConfigSync',
     value: function loadConfigSync(name) {
-      return _loadConfigSync(this._dirname, name);
+      var _nameType4 = t.string();
+
+      var _returnType4 = t.return(t.ref('Map', t.string(), t.any()));
+
+      t.param('name', _nameType4).assert(name);
+
+      return _returnType4.assert(_loadConfigSync(this._dirname, name));
     }
   }]);
 
   return Config;
-}();
+}(), (_descriptor = _applyDecoratedDescriptor(_class.prototype, '_map', [_dec], {
+  enumerable: true,
+  initializer: null
+}), _descriptor2 = _applyDecoratedDescriptor(_class.prototype, '_dirname', [_dec2], {
+  enumerable: true,
+  initializer: null
+}), _descriptor3 = _applyDecoratedDescriptor(_class.prototype, 'packageConfig', [_dec3], {
+  enumerable: true,
+  initializer: null
+})), _class));
 
 export default function alpConfig(dirname) {
   var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
+  var _dirnameType4 = t.nullable(t.string());
+
+  t.param('dirname', _dirnameType4).assert(dirname);
+  t.param('options', ConfigOptions).assert(options);
+
   return function (app, config) {
+    var _configType = t.nullable(t.ref(Config));
+
+    t.param('config', _configType).assert(config);
+
     if (!config) {
-      config = new Config(dirname);
+      config = _configType.assert(new Config(dirname));
       config.loadSync(options);
     }
 
