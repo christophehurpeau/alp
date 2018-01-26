d4e0212401ae9bc06bbfa9c2dc5cf29318ecb158
Christophe Hurpeau <christophe@hurpeau.com>
2018-01-26 12:31:27 +0100

    fix: missing limit in migrate script

--- packages/alp-auth/scripts/migrate/rethink2mongo.js
+++ packages/alp-auth/scripts/migrate/rethink2mongo.js
@@ -5,7 +5,10 @@ export default async (rethinkConnection, mongoConnection) => {
   const rethinkUsersStore = new RethinkStore(rethinkConnection, 'users');
   const mongoUsersStore = new MongoStore(mongoConnection, 'users');
 
-  const usersCursor = await rethinkUsersStore.query().run({ cursor: true });
+  const usersCursor = await rethinkUsersStore
+    .query()
+    .limit(99999)
+    .run({ cursor: true });
 
   return usersCursor.eachAsync(async (row, rowFinished) => {
     row._id = row.id;
