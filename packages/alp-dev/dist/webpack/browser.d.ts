d1fcc6147e93e202388932f76abddc826fbf214a
Christophe Hurpeau <christophe@hurpeau.com>
2019-04-09 09:23:15 +0200

    feat: update alp-dev browser.d.ts

--- packages/alp-dev/dist/webpack/browser.d.ts
+++ packages/alp-dev/dist/webpack/browser.d.ts
@@ -3,5 +3,5 @@ import { PobpackCompiler } from 'pobpack-types';
 export declare const createModernBrowserCompiler: (production: boolean) => PobpackCompiler;
 export declare const createOlderBrowserCompiler: (production: boolean) => PobpackCompiler;
 export declare type RunDevServerOptions = Pick<RunOptions, Exclude<keyof RunOptions, 'port' | 'https' | 'headers' | 'proxy'>>;
-export declare const runDevServer: (compiler: PobpackCompiler, port: number, proxyPort: number, options?: Pick<RunOptions, "public" | "index" | "after" | "allowedHosts" | "bonjour" | "clientLogLevel" | "contentBase" | "disableHostCheck" | "filename" | "historyApiFallback" | "host" | "hotOnly" | "inline" | "lazy" | "noInfo" | "open" | "openPage" | "pfx" | "pfxPassphrase" | "publicPath" | "setup" | "socket" | "sockPath" | "staticOptions" | "stats" | "useLocalIp" | "watchContentBase" | "watchOptions" | "writeToDisk"> | undefined) => any;
+export declare const runDevServer: (compiler: PobpackCompiler, port: number, proxyPort: number, options?: Pick<RunOptions, string | number> | undefined) => any;
 //# sourceMappingURL=browser.d.ts.map
