ccb141d4dc1234ed4a03bc9f12436ab0ea20f74f
Christophe Hurpeau <christophe@hurpeau.com>
2019-04-20 00:54:46 +0200

    refactor: remove deprecated react-alp-login and react-alp-link

--- packages/react-alp-login/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as LoginButtons } from './LoginButtons';
-export { default as LoginButtonGoogle } from './LoginButtonGoogle';
-export { default as LoginButtonSlack } from './LoginButtonSlack';
-//# sourceMappingURL=index.d.ts.map
--- packages/react-alp-login/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LoginButtons } from './LoginButtons';
-export { default as LoginButtonGoogle } from './LoginButtonGoogle';
-export { default as LoginButtonSlack } from './LoginButtonSlack';
-// export { default as LoginForm } from './LoginForm';
