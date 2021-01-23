344ad84e4913f860f42d556cad282a8dd280d867
Christophe Hurpeau <christophe@hurpeau.com>
2021-01-23 21:43:48 +0100

    fix(react-alp-antd-form): typings

--- packages/react-alp-antd-form/dist/FormField.d.ts
+++ packages/react-alp-antd-form/dist/FormField.d.ts
@@ -1,15 +1,14 @@
 import { Form } from 'antd';
 import type { ComponentProps, ComponentType, ReactElement } from 'react';
-import { Field } from 'react-final-form';
+import type { UseFieldConfig } from 'react-final-form';
 declare type FormItemProps = ComponentProps<typeof Form.Item>;
-declare type FieldProps = ComponentProps<typeof Field>;
-interface FormFieldProps<P extends Record<string, never>> {
+interface FormFieldProps<FieldValue, P> extends UseFieldConfig<FieldValue> {
     component: ComponentType<P>;
     label: FormItemProps['label'];
     help: FormItemProps['help'];
-    id: string;
-    name: FieldProps['name'];
+    name: string;
+    id?: string;
 }
-export default function FormField<P extends Record<string, never>>({ component: Component, label, name, id, help, ...props }: FormFieldProps<P>): ReactElement;
+export default function FormField<FieldValue, P>({ component: Component, label, name, id, help, ...props }: FormFieldProps<FieldValue, P> & Omit<P, keyof FormFieldProps<FieldValue, P>>): ReactElement;
 export {};
 //# sourceMappingURL=FormField.d.ts.map
--- packages/react-alp-antd-form/src/FormField.tsx
+++ packages/react-alp-antd-form/src/FormField.tsx
@@ -1,30 +1,31 @@
 import { Form } from 'antd';
 import type { ComponentProps, ComponentType, ReactElement } from 'react';
 import React from 'react';
+import type { UseFieldConfig } from 'react-final-form';
 import { Field } from 'react-final-form';
 
 type FormItemProps = ComponentProps<typeof Form.Item>;
-type FieldProps = ComponentProps<typeof Field>;
 
-interface FormFieldProps<P extends Record<string, never>> {
+interface FormFieldProps<FieldValue, P> extends UseFieldConfig<FieldValue> {
   component: ComponentType<P>;
   label: FormItemProps['label'];
   help: FormItemProps['help'];
-  id: string;
-  name: FieldProps['name'];
+  name: string;
+  id?: string;
 }
 
-export default function FormField<P extends Record<string, never>>({
+export default function FormField<FieldValue, P>({
   component: Component,
   label,
   name,
   id = name,
   help,
   ...props
-}: FormFieldProps<P>): ReactElement {
+}: FormFieldProps<FieldValue, P> &
+  Omit<P, keyof FormFieldProps<FieldValue, P>>): ReactElement {
   return (
     <Form.Item htmlFor={id} label={label} help={help}>
-      <Field
+      <Field<FieldValue>
         id={id}
         name={name}
         render={({ input, meta, ...rest }): ReactElement => (
