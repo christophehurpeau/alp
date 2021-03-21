b4dae49e6886517ad57976ec4a5b04d7e8f6b8aa
Christophe Hurpeau <christophe@hurpeau.com>
2021-03-21 17:00:34 +0100

    fix(alp-node-auth): better strategies.d.ts

--- packages/alp-node-auth/strategies/strategies.d.ts
+++ packages/alp-node-auth/strategies/strategies.d.ts
@@ -1,57 +1,9 @@
 import { Config, SlackParams } from 'alp-types';
 import { OAuthClient } from 'simple-oauth2';
 
-declare module 'alp-node-auth/strategies/dropbox' {
-  export interface DropboxStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient;
-  }
-
-  export default function createDropboxStrategy(
-    config: Config,
-  ): DropboxStrategy;
+export interface Strategy {
+  type: 'oauth2';
+  oauth2: OAuthClient<SlackParams>;
 }
-declare module 'alp-node-auth/strategies/facebook' {
-  export interface FacebookStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient;
-  }
 
-  export default function createFacebookStrategy(
-    config: Config,
-  ): FacebookStrategy;
-}
-declare module 'alp-node-auth/strategies/foursquare' {
-  export interface FoursquareStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient;
-  }
-
-  export default function createFoursquareStrategy(
-    config: Config,
-  ): FoursquareStrategy;
-}
-declare module 'alp-node-auth/strategies/github' {
-  export interface GithubStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient;
-  }
-
-  export default function createGithubStrategy(config: Config): GithubStrategy;
-}
-declare module 'alp-node-auth/strategies/google' {
-  export interface GoogleStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient;
-  }
-
-  export default function createGoogleStrategy(config: Config): GoogleStrategy;
-}
-declare module 'alp-node-auth/strategies/slack' {
-  export interface SlackStrategy {
-    type: 'oauth2';
-    oauth2: OAuthClient<SlackParams>;
-  }
-
-  export default function createSlackStrategy(config: Config): SlackStrategy;
-}
+export default function createStrategy(config: Config): Strategy;
