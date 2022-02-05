4bcc2a69d7ecb416f63c707a33237bc868e85696
Christophe Hurpeau <christophe@hurpeau.com>
2022-02-05 22:50:26 +0100

    fix: alp-node/fetch typings

--- packages/alp-node/src/fetch.ts
+++ packages/alp-node/src/fetch.ts
@@ -1,10 +1,6 @@
 import nodeFetch from 'node-fetch';
 
-declare global {
-  // eslint-disable-next-line vars-on-top, no-var
-  var fetch: typeof nodeFetch;
-}
-
+// @ts-expect-error -- node-fetch does not exactly match dom api
 global.fetch = nodeFetch;
 
 // eslint-disable-next-line unicorn/prefer-export-from
