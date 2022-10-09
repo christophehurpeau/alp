f411dc8bd24c0d5d889163b01825a46b2e36f88e
Christophe Hurpeau <christophe@hurpeau.com>
2022-10-10 00:38:46 +0200

    test: fix build id different each time build

--- packages/alp-hello-react-example/src/HomeView/components/InputNameComponent.tsx
+++ packages/alp-hello-react-example/src/HomeView/components/InputNameComponent.tsx
@@ -2,12 +2,6 @@ import { Input } from 'native-base';
 import type { ComponentProps, KeyboardEventHandler, ReactElement } from 'react';
 import { useState } from 'react';
 
-// const Input = styled.input`
-//   width: 100%;
-//   font-size: 1.5rem;
-//   margin-top: 2rem;
-// `;
-
 interface InputNameComponentProps {
   value: undefined | string;
   onChange: (name: string) => void;
@@ -20,8 +14,8 @@ export default function InputNameComponent({
   const [value, setValue] = useState(initialValue);
 
   const handleChange: ComponentProps<typeof Input>['onChange'] = (e) => {
-    setValue(e.target.value);
-    onChange(e.target.value);
+    setValue(e.nativeEvent.text);
+    onChange(e.nativeEvent.text);
   };
 
   const handleKeyUp: KeyboardEventHandler<HTMLInputElement> = (e) => {
