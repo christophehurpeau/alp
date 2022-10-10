7b340fa45217eae9c26b23ca07e2822537f39de2
Christophe Hurpeau <christophe@hurpeau.com>
2022-10-10 20:16:53 +0200

    fix: tsc

--- packages/alp-hello-react-example/src/HomeView/components/InputNameComponent.tsx
+++ packages/alp-hello-react-example/src/HomeView/components/InputNameComponent.tsx
@@ -1,5 +1,5 @@
 import { Input } from 'native-base';
-import type { ComponentProps, KeyboardEventHandler, ReactElement } from 'react';
+import type { ReactElement } from 'react';
 import { useState } from 'react';
 
 interface InputNameComponentProps {
@@ -13,23 +13,16 @@ export default function InputNameComponent({
 }: InputNameComponentProps): ReactElement {
   const [value, setValue] = useState(initialValue);
 
-  const handleChange: ComponentProps<typeof Input>['onChange'] = (e) => {
-    setValue(e.nativeEvent.text);
-    onChange(e.nativeEvent.text);
-  };
-
-  const handleKeyUp: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    setValue(e.currentTarget.value);
-  };
-
   return (
     <Input
       data-testid="input-name"
       type="text"
       autoComplete="off"
       value={value}
-      onChange={handleChange}
-      onKeyUp={handleKeyUp}
+      onChangeText={(newValue) => {
+        setValue(newValue);
+        onChange(newValue);
+      }}
     />
   );
 }
