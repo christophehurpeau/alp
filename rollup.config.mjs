e9cbde74ddbbb730bc2b65bb6d0b87f2bba8006e
Christophe Hurpeau <christophe@hurpeau.com>
2023-01-29 20:21:03 +0100

    feat: update configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,24 @@
 import configs1 from './packages/alp-body-parser/rollup.config.mjs';
 import configs2 from './packages/alp-nextjs/rollup.config.mjs';
-import configs4 from './packages/react-alp-connection-state/rollup.config.mjs';
-import configs5 from './packages/alp-migrations/rollup.config.mjs';
-import configs6 from './packages/alp-node-config/rollup.config.mjs';
-import configs7 from './packages/alp-node-errors/rollup.config.mjs';
-import configs8 from './packages/alp-node-language/rollup.config.mjs';
-import configs9 from './packages/alp-router/rollup.config.mjs';
-import configs10 from './packages/react-alp-context/rollup.config.mjs';
-import configs11 from './packages/alp-listen/rollup.config.mjs';
-import configs12 from './packages/alp-params/rollup.config.mjs';
-import configs14 from './packages/alp-translate/rollup.config.mjs';
-import configs15 from './packages/react-alp-loading-bar/rollup.config.mjs';
-import configs16 from './packages/react-alp-translate/rollup.config.mjs';
-import configs17 from './packages/alp-node/rollup.config.mjs';
-import configs18 from './packages/alp-node-auth/rollup.config.mjs';
+import configs3 from './packages/react-alp-connection-state/rollup.config.mjs';
+import configs4 from './packages/alp-migrations/rollup.config.mjs';
+import configs5 from './packages/alp-node-config/rollup.config.mjs';
+import configs6 from './packages/alp-node-errors/rollup.config.mjs';
+import configs7 from './packages/alp-node-language/rollup.config.mjs';
+import configs8 from './packages/alp-router/rollup.config.mjs';
+import configs9 from './packages/react-alp-context/rollup.config.mjs';
+import configs10 from './packages/alp-listen/rollup.config.mjs';
+import configs11 from './packages/alp-params/rollup.config.mjs';
+import configs12 from './packages/alp-translate/rollup.config.mjs';
+import configs13 from './packages/react-alp-loading-bar/rollup.config.mjs';
+import configs14 from './packages/react-alp-translate/rollup.config.mjs';
+import configs15 from './packages/alp-node/rollup.config.mjs';
+import configs16 from './packages/alp-node-auth/rollup.config.mjs';
 
 export default [
   ...configs1,
   ...configs2,
+  ...configs3,
   ...configs4,
   ...configs5,
   ...configs6,
@@ -27,9 +28,8 @@ export default [
   ...configs10,
   ...configs11,
   ...configs12,
+  ...configs13,
   ...configs14,
   ...configs15,
   ...configs16,
-  ...configs17,
-  ...configs18,
 ];
