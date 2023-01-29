eb764f4266c2ca505e6b9ceefe507054c485a9d5
Christophe Hurpeau <christophe@hurpeau.com>
2023-01-29 19:34:43 +0100

    feat: update to next 13

--- packages/alp-hello-react-example/next.config.js
+++ packages/alp-hello-react-example/next.config.js
@@ -1,5 +1,3 @@
-import withPlugins from 'next-compose-plugins';
-import withTM from 'next-transpile-modules';
 // import yoRcConfig from '../../.yo-rc.json';
 
 /** @type {import('next').NextConfig} */
@@ -19,6 +17,31 @@ const nextConfig = {
       unoptimized: true,
     },
   },
+  transpilePackages: [
+    // native-base - https://github.com/GeekyAnts/nativebase-templates/blob/master/nextjs-with-native-base/next.config.js
+    'native-base',
+    'react-native-svg',
+    'react-native-web',
+    'react-native-safe-area-context',
+    '@react-aria/visually-hidden',
+    '@react-native-aria/button',
+    '@react-native-aria/checkbox',
+    '@react-native-aria/combobox',
+    '@react-native-aria/focus',
+    '@react-native-aria/interactions',
+    '@react-native-aria/listbox',
+    '@react-native-aria/overlays',
+    '@react-native-aria/radio',
+    '@react-native-aria/slider',
+    '@react-native-aria/tabs',
+    '@react-native-aria/utils',
+    '@react-stately/combobox',
+    '@react-stately/radio',
+
+    // requires react-native-web
+    'alp-nextjs',
+    'react-alp-connection-state',
+  ],
   webpack: (config, context) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -37,33 +60,4 @@ if (process.env.TEST_BUILD_ID) {
   nextConfig.generateBuildId = () => process.env.TEST_BUILD_ID;
 }
 
-export default withPlugins(
-  [
-    withTM([
-      // native-base - https://github.com/GeekyAnts/nativebase-templates/blob/master/nextjs-with-native-base/next.config.js
-      'native-base',
-      'react-native-svg',
-      'react-native-web',
-      'react-native-safe-area-context',
-      '@react-aria/visually-hidden',
-      '@react-native-aria/button',
-      '@react-native-aria/checkbox',
-      '@react-native-aria/combobox',
-      '@react-native-aria/focus',
-      '@react-native-aria/interactions',
-      '@react-native-aria/listbox',
-      '@react-native-aria/overlays',
-      '@react-native-aria/radio',
-      '@react-native-aria/slider',
-      '@react-native-aria/tabs',
-      '@react-native-aria/utils',
-      '@react-stately/combobox',
-      '@react-stately/radio',
-
-      // requires react-native-web
-      'alp-nextjs',
-      'react-alp-connection-state',
-    ]),
-  ],
-  nextConfig,
-);
+export default nextConfig;
