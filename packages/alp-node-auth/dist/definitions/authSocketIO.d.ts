d65732424716e30d421fce30f04713ab75464487
Christophe Hurpeau <christophe@hurpeau.com>
2023-02-18 16:08:55 +0100

    build: build dist

--- packages/alp-node-auth/dist/definitions/authSocketIO.d.ts
+++ packages/alp-node-auth/dist/definitions/authSocketIO.d.ts
@@ -1,6 +1,5 @@
 import type { NodeApplication } from 'alp-types';
-import type { Option } from 'cookies';
 import type { User } from '../types.d';
 import type MongoUsersManager from './MongoUsersManager';
-export declare const authSocketIO: <U extends User = User>(app: NodeApplication, usersManager: MongoUsersManager<U, import("../types.d").UserSanitized>, io: any, options?: Pick<Option, Exclude<keyof Option, 'secure'>>) => void;
+export declare const authSocketIO: <U extends User = User>(app: NodeApplication, usersManager: MongoUsersManager<U, import("../types.d").UserSanitized>, io: any) => void;
 //# sourceMappingURL=authSocketIO.d.ts.map
