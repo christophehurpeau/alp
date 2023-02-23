49b2545406afc2d29507e313c37796138e53019f
Christophe Hurpeau <christophe@hurpeau.com>
2023-02-24 00:03:02 +0100

    docs(alp-node): add todo for node 18

--- packages/alp-node/src/fetch.ts
+++ packages/alp-node/src/fetch.ts
@@ -1,5 +1,8 @@
 import nodeFetch from 'node-fetch';
 
+// TODO remove with node 18 since is native
+// https://nodejs.org/en/blog/announcements/v18-release-announce/
+
 // @ts-expect-error -- node-fetch does not exactly match dom api
 global.fetch = nodeFetch;
 
