a17b938886e5d6ae54b29a50d3fda478e4e4cf30
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-07-04 21:39:24 +0200

    chore: update dev dependencies and use workspaces

--- scripts/check-packages.js
+++ scripts/check-packages.js
@@ -3,18 +3,5 @@ import { createCheckPackageWithWorkspaces } from "check-package-dependencies";
 await createCheckPackageWithWorkspaces({
   isLibrary: (pkg) => !pkg.name.endsWith("-example"),
 })
-  .checkRecommended({
-    onlyWarnsForInMonorepoPackagesDependencies: {
-      "alp-migrations": {
-        "*": {
-          duplicateDirectDependency: ["nightingale-logger"],
-        },
-      },
-      "alp-node-auth": {
-        "*": {
-          duplicateDirectDependency: ["nightingale-logger"],
-        },
-      },
-    },
-  })
+  .checkRecommended()
   .run();
