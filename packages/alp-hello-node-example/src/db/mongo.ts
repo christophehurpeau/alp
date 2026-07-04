0a01c5b313974598a1e176fe01e1498c209a0bf0
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-07-04 21:45:10 +0200

    fix(alp-hello-node-example): fix MongoConfig

--- packages/alp-hello-node-example/src/db/mongo.ts
+++ packages/alp-hello-node-example/src/db/mongo.ts
@@ -1,16 +1,11 @@
 // eslint-disable-next-line import-x/no-unresolved
 import { config } from "alp-node";
-import type { MongoBaseModel } from "liwi-mongo";
+import type { MongoBaseModel, MongoConfig } from "liwi-mongo";
 // eslint-disable-next-line import-x/no-unresolved
 import { MongoConnection, MongoStore } from "liwi-mongo";
 
 export const mongoConnection: MongoConnection = new MongoConnection(
-  new Map(
-    Object.entries(
-      config.get<Record<"mongodb", Record<string, number | string>>>("db")
-        .mongodb,
-    ).map(([key, value]) => [key, value]),
-  ),
+  config.get<Record<"mongodb", MongoConfig>>("db").mongodb,
 );
 
 export const createMongoStore = <Model extends MongoBaseModel>(
