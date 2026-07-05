5b91fec9ac2ff6b19c62c8f999cfa05ab4540054
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-07-05 08:39:38 +0200

    chore: remove react-alp-loading-bar package and its related files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,4 @@
 import configs1 from "./packages/alp-node/rollup.config.mjs";
-import configs3 from "./packages/react-alp-loading-bar/rollup.config.mjs";
 import configs4 from "./packages/alp-body-parser/rollup.config.mjs";
 import configs5 from "./packages/alp-migrations/rollup.config.mjs";
 import configs6 from "./packages/alp-node-auth/rollup.config.mjs";
@@ -7,7 +6,6 @@ import configs7 from "./packages/alp-hello-node-example/rollup.config.mjs";
 
 export default [
   ...configs1,
-  ...configs3,
   ...configs4,
   ...configs5,
   ...configs6,
